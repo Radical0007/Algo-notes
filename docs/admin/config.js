e91c866540ae1f221e69415996e99a2484c5fff8
Radical0007 <3534709623@qq.com>
2026-08-10 19:30:12 +0800

    Configure deployed upload worker

--- docs/admin/config.js
+++ docs/admin/config.js
@@ -1,4 +1,3 @@
 window.ALGO_NOTES_ADMIN_CONFIG = Object.freeze({
-  // Fill this after deploying worker/: https://your-worker.workers.dev
-  apiBaseUrl: ""
+  apiBaseUrl: "https://algo-notes-upload.radical0007.workers.dev"
 });
